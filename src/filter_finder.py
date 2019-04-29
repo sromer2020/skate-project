b37189d5fe18ea833f3e04cac40edff00742643a
Nicholas Muise <nmuise@cs.hartford.edu>
2019-04-29 01:31:31 -0400

    made filter_finder.py object-oriented for easier future configurability

--- src/filter_finder.py
+++ src/filter_finder.py
@@ -1,11 +1,8 @@
 """
-This script is a diagnostic tool intended for the manual determination of what color ranges can be used to threshold out all pixels corresponding to a specific skateboard in videos (or snippets of videos) in which a single skateboard is filmed under controlled, static lighting conditions.
-
-As of 04/22/2019: In order to use this script properly, the values for path, lower_chosen, and upper_chosen must be manually edited into this code.
-
-TODO: make this script configurable to run from the commandline with no need to edit code, e.g. through `python board_filter -v "board.mp4" -c "wheels"`
-
-This script was created as part of a school project, and therefore license-free.
+This class is a diagnostic tool used for the manual determination of what color 
+ranges can be used to threshold out all pixels corresponding to a specific
+semantic features in videos (or snippets of videos) in which a single thing is 
+filmed under controlled, static lighting conditions.
 """
 
 import os.path
@@ -14,152 +11,198 @@ import numpy as np
 
 __author__ = "Nick"
 
+class FilterFinder:
 
-#Increments by which values can be changed through keyboard controls
-sinc = 1
-binc = 5
+	# TODO: add some basic validation and related exception handling (?)
+	def __init__(self, unknown_filters = None, known_filters = None):
+		# Increments by which values can be changed through keyboard controls
+		self.increments = [1, 5]
 
-path = 'We have to manually enter the path to the video file here until we add support for commandline parameters.' #TODO: make path passable from the commandline
+		# quadruples of (control key, H/S/V channel index, sign of increment/decrement,
+		# index of increment to use
+		self.lower_controls = [	('a', 0, 1, 0),
+								('s', 1, 1, 0),
+								('d', 2, 1, 0),
+								('z', 0, -1, 0),
+								('x', 1, -1, 0),
+								('c', 2, -1, 0),
 
-#if path leads to valid file
-if os.path.isfile(path):
-	cap = cv2.VideoCapture(path)
-	
-	#try to read first frame
-	success, frame = cap.read()
-	
-	if(success):
-		#get dimensions of this video
-		height, width, _ = frame.shape
+								('A', 0, 1, 1),
+								('S', 1, 1, 1),
+								('D', 2, 1, 1),
+								('Z', 0, -1, 1),
+								('X', 1, -1, 1),
+								('C', 2, -1, 1)]
 
-		#lower and upper bounds for color detections, to be derived using keyboard controls. The default values given here are the averages of previous trials with Steve's skateboard.
-		lower_board = np.array([82,39,80])
-		upper_board = np.array([100,155,180])
+		self.upper_controls = [	('g', 0, 1, 0),
+								('h', 1, 1, 0),
+								('j', 2, 1, 0),
+								('b', 0, -1, 0),
+								('n', 1, -1, 0),
+								('m', 2, -1, 0),
 
-		lower_wheel = np.array([10,40,0])
-		upper_wheel = np.array([29,230,255])
+								('G', 0, 1, 1),
+								('H', 1, 1, 1),
+								('J', 2, 1, 1),
+								('B', 0, -1, 1),
+								('N', 1, -1, 1),
+								('M', 2, -1, 1)]
+		
+		# initialize filters 
+		self.filters = {}
+		if unknown_filters is not None:
+			self.filters = self.get_filter_map(unknown_filters)
+		if known_filters is not None:
+			self.set_filters(known_filters)
+		# if both of the above conditions failed, use generic filter names
+		if self.filters == {}:
+			self.filters = self.get_filter_map("filter1", "filter2")
 
-		lower_edge = np.array([10,25,115])
-		upper_edge = np.array([25,155,255])
-
-		#decide which color range to experiment on during this run
-		lower_chosen = lower_edge
-		upper_chosen = upper_edge
-
-		#factor by which to downsample each frame
-		scale = 2
-
-		#go through all the frames of this video
-		while(success):
-			#rescale frame for faster processing
-			frame = cv2.resize(frame, (width / scale, height / scale))
-			
-			#convert frame to HSV for easier processing
-			hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-			
-			#get masks for individual board features
-			board_mask = cv2.inRange(hsv_frame, lower_board, upper_board)
-			wheel_mask = cv2.inRange(hsv_frame, lower_wheel, upper_wheel)
-			edge_mask = cv2.inRange(hsv_frame, lower_edge, upper_edge)
-			
-			#combine feature masks into one
-			skateboard_mask = cv2.bitwise_or(board_mask, wheel_mask)
-			skateboard_mask = cv2.bitwise_or(skateboard_mask, edge_mask)
-			
-			#Extract features from frame using masks
-			extracted_board = cv2.bitwise_and(frame, frame, mask = board_mask)
-			extracted_wheel = cv2.bitwise_and(frame, frame, mask = wheel_mask)
-			extracted_edge = cv2.bitwise_and(frame, frame, mask = edge_mask)
-			extracted_skateboard = cv2.bitwise_and(frame, frame, mask = skateboard_mask)
-			
-			#show raw frame
-			cv2.imshow('raw frame', frame)
-
-			#show all extracted features
-			cv2.imshow('extracted board', extracted_board)
-			cv2.imshow('extracted wheels', extracted_wheel)
-			cv2.imshow('extracted edges', extracted_edge)
-			cv2.imshow('extracted skateboard', extracted_skateboard)
-			
-			#exit early if escape key pressed
-			k = cv2.waitKey(5) & 0xFF
-			if k == 27:
-				break
-
-			#keyboard controls for altering the lower bounds of experimental color range
-			if k == ord('a'):
-				lower_chosen[0] = (lower_chosen[0] + sinc) % 256
-			if k == ord('s'):
-				lower_chosen[1] = (lower_chosen[1] + sinc) % 256
-			if k == ord('d'):
-				lower_chosen[2] = (lower_chosen[2] + sinc) % 256
-			if k == ord('z'):
-				lower_chosen[0] = (lower_chosen[0] - sinc) % 256
-			if k == ord('x'):
-				lower_chosen[1] = (lower_chosen[1] - sinc) % 256
-			if k == ord('c'):
-				lower_chosen[2] = (lower_chosen[2] - sinc) % 256
+	# display video at path and allow user to alter chosen color range live
+	def find_filter(self, path, chosen, downsize_scale = 2, write = True):
+		# if path leads to valid file
+		if os.path.isfile("../videos/Board_trim1.mp4"):
+			# if desired parameter exists in filtering parameter set
+			if chosen in self.filters:
+				# decide which color filter bounds to alter using controls based on supplied name
+				lower_chosen, upper_chosen = self.filters[chosen][0], self.filters[chosen][1]
 				
-			if k == ord('A'):
-				lower_chosen[0] = (lower_chosen[0] + binc) % 256
-			if k == ord('S'):
-				lower_chosen[1] = (lower_chosen[1] + binc) % 256
-			if k == ord('D'):
-				lower_chosen[2] = (lower_chosen[2] + binc) % 256
-			if k == ord('Z'):
-				lower_chosen[0] = (lower_chosen[0] - binc) % 256
-			if k == ord('X'):
-				lower_chosen[1] = (lower_chosen[1] - binc) % 256
-			if k == ord('C'):
-				lower_chosen[2] = (lower_chosen[2] - binc) % 256
-
-			#keyboard controls for altering the upper bounds of experimental color range
-			if k == ord('g'):
-				upper_chosen[0] = (upper_chosen[0] + sinc) % 256
-			if k == ord('h'):
-				upper_chosen[1] = (upper_chosen[1] + sinc) % 256
-			if k == ord('j'):
-				upper_chosen[2] = (upper_chosen[2] + sinc) % 256
-			if k == ord('b'):
-				upper_chosen[0] = (upper_chosen[0] - sinc) % 256
-			if k == ord('n'):
-				upper_chosen[1] = (upper_chosen[1] - sinc) % 256
-			if k == ord('m'):
-				upper_chosen[2] = (upper_chosen[2] - sinc) % 256
-
-			if k == ord('G'):
-				upper_chosen[0] = (upper_chosen[0] + binc) % 256
-			if k == ord('H'):
-				upper_chosen[1] = (upper_chosen[1] + binc) % 256
-			if k == ord('J'):
-				upper_chosen[2] = (upper_chosen[2] + binc) % 256
-			if k == ord('B'):
-				upper_chosen[0] = (upper_chosen[0] - binc) % 256
-			if k == ord('N'):
-				upper_chosen[1] = (upper_chosen[1] - binc) % 256
-			if k == ord('M'):
-				upper_chosen[2] = (upper_chosen[2] - binc) % 256
+				# try to read file
+				cap = cv2.VideoCapture(path)
+				success, frame = cap.read()
 				
-			#keyboard controls to reset experimental color range
-			if k == ord('r'):
-				lower_chosen[0] = 0
-				lower_chosen[1] = 0
-				lower_chosen[2] = 0
-				upper_chosen[0] = 255
-				upper_chosen[1] = 255
-				upper_chosen[2] = 255
+				if success:
+					# get dimensions of this video
+					height, width, _ = frame.shape
+					scaled_height, scaled_width = height/downsize_scale, width/downsize_scale
 
-			#advance frame
-			success, frame = cap.read()
-			
-		cv2.destroyAllWindows()
+					# parameters for displaying filter parameters on extracted frames
+					font = cv2.FONT_HERSHEY_SIMPLEX
+					font_position = (50,scaled_height - 40)
+					font_scale = .5
+					font_color = (0,0,255)
+					font_thickness = 2
+					
+					# dictionary of all extracted features to be displayed during runtime
+					features = {}
+					# dictionary of all binary masks used to extract those features
+					masks = {}
+					
+					# go through all the frames of this video, displaying extracted features
+					while success:
+						# rescale frame for faster processing
+						frame = cv2.resize(frame, (scaled_width, scaled_height))
+						hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+						
+						features["raw"] = frame
+						
+						# get all masks and features for each filter
+						for key, (lower,upper) in self.filters.iteritems():
+							# create mask
+							masks[key] = cv2.inRange(hsv_frame, lower, upper)
+							# extract feature using mask
+							feature = cv2.bitwise_and(frame, frame, mask = masks[key])
+							
+							# write current filter info onto each filtered feature
+							lower_text = "lower: [{0},{1},{2}]".format(lower[0], lower[1], lower[2])
+							upper_text = "upper: [{0},{1},{2}]".format(upper[0],upper[1],upper[2])
+							cv2.putText(feature, lower_text, font_position, font, font_scale, font_color, font_thickness)
+							cv2.putText(feature, upper_text, (font_position[0], font_position[1]-30), font, font_scale, font_color, font_thickness)
 
-		#print out final values of experimental parameters once program is terminated
-		print "Parameters derived during this run:"
-		print "\r\n---Lower and upper bounds on color range:"
-		print "[{},{},{}]".format(lower_chosen[0], lower_chosen[1], lower_chosen[2])
-		print "[{},{},{}]".format(upper_chosen[0], upper_chosen[1], upper_chosen[2])
+							features[key] = feature
+
+						# reset combined mask before recalculating it
+						# TODO: find a way around having to do this
+						masks["combined"] = 0
+						masks["combined"] = self.combine_masks(masks)
+						
+						features["combined"] = cv2.bitwise_and(frame, frame, mask = masks["combined"])
+						
+						# show all things
+						for key, feature in features.iteritems():
+							cv2.imshow(key, feature)
+						
+						# exit early if escape key pressed
+						k = cv2.waitKey(5) & 0xFF
+						if k == 27:
+							break
+
+						# use defined keyboard controls to update upper and lower bounds of chosen color range
+
+						# update lower bounds
+						for key, channel, sign, increment in self.lower_controls:
+							if k == ord(key):
+								lower_chosen[channel] = (lower_chosen[channel] + (sign * self.increments[increment])) % 256
+						# update upper bounds
+						for key, channel, sign, increment in self.upper_controls:
+							if k == ord(key):
+								upper_chosen[channel] = (upper_chosen[channel] + (sign * self.increments[increment])) % 256
+						
+						# keyboard controls to reset experimental color range
+						if k == ord('r'):
+							lower_chosen[:] = 0
+							upper_chosen[:] = 255
+						if k == ord('R'):
+							lower_chosen[:] = 0
+							upper_chosen[:] = 0
+
+						# advance frame
+						success, frame = cap.read()
+						
+					cv2.destroyAllWindows()
+					
+					# update values of this filter if desired
+					if (write):
+						self.filters[chosen][0], self.filters[chosen][1] = lower_chosen, upper_chosen
+					return (lower_chosen, upper_chosen)
+				# TODO: implement actual exception/error handling
+				else: print "The file provided could not be read. Make sure the given path leads to a video file."
+			else: 
+				print ("The desired filtering parameter \"{0}\" was not found "+
+				"in the parameter set:\r\n{1}").format(chosen, self.filters.keys())
+		else: print "The supplied filepath did not lead to a file."
 	
-	else: print "The file provided could not be read. Make sure the given path leads to a video file."
+	# return a set of predetermined filters based on the trials using Steven's skateboard
+	def get_default_filters(self):
+		return {"board":[np.array([82,39,80]), np.array([100,155,180])], 
+		"wheel":[np.array([10,40,0]),np.array([29,230,255])],
+		"edge":[np.array([10,25,115]), np.array([25,155,255])]}
 	
-else: print "please enter a valid filepath."
+	# map a single name, or list of names to a default set of filter parameters
+	def get_filter_map(self, names, min_style_default = True):
+		# decide which range to copy into all filters by default
+		default = []
+		if min_style_default is False:
+			default = [[0,0,0],[255,255,255]]
+		else: default = [[0,0,0],[0,0,0]]
+		
+		# if names is not list, put names into list
+		# TODO: is this solution actually problematic?
+		if not isinstance(names, list):
+			names = [names]
+		
+		# map names to generic filter parameters
+		filters = {}
+		for name in names:
+			filters[name] = [np.array(default[0][:]),np.array(default[1][:])]
+		return filters
+
+	def export_params(self):
+		return self.filters
+
+	# combine arbitrary number of binary masks into one
+	def combine_masks(self, masks):
+		combined_mask = []
+		for key, mask in masks.iteritems():
+			# initialize result mask to be first mask in dict if it doesn't already have a value
+			# TODO: find a good solution to get rid of this ugly garbage solution
+			if combined_mask == []:
+				combined_mask = mask
+				continue
+			combined_mask = cv2.bitwise_or(combined_mask, mask)
+		return combined_mask
+	
+	# add all filters from a given dictionary of name: [lower, upper] to this instance's filters
+	def set_filters(self, filters):
+		for name, filter in filters.iteritems():
+			self.filters[name] = filter
