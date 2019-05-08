2e229dfe3ded8b155b4ead5df97111244706a87e
Nicholas Muise <nmuise@cs.hartford.edu>
2019-05-08 14:04:57 -0400

    Made filter-finding process more user-friendly
    FilterFinder.find_filter now loops through supplied video until user is satisfied with current params
    
    Added FilterFinder.find_filters to allow user to find multiple filters in one go

--- src/filter_finder.py
+++ src/filter_finder.py
@@ -51,12 +51,12 @@ class FilterFinder:
 		# initialize filters 
 		self.filters = {}
 		if unknown_filters is not None:
-			self.set_filters(self.get_filter_map(unknown_filters))
+			self.set_filters(self.add_filter_maps(unknown_filters))
 		if known_filters is not None:
 			self.set_filters(known_filters)
 		# if both of the above conditions failed, use generic filter names
 		if self.filters == {}:
-			self.filters = self.get_filter_map('filter1', 'filter2')
+			self.filters = self.add_filter_maps('filter1', 'filter2')
 
 	# display video at path and allow user to alter chosen color range live
 	# TODO: chunk this method into smaller methods because it's pretty long
@@ -68,101 +68,118 @@ class FilterFinder:
 				# decide which color filter bounds to alter using controls based on supplied name
 				lower_chosen, upper_chosen = self.filters[chosen][0], self.filters[chosen][1]
 				
-				# try to read file
-				cap = cv2.VideoCapture(path)
-				success, frame = cap.read()
-				
-				if success:
-					# get dimensions of this video
-					height, width, _ = frame.shape
-					scaled_height, scaled_width = height/downsize_scale, width/downsize_scale
+				# loop through video repeatedly until user is satisfied with filter params	
+				done = 'n'
+				while done != 'y':
+					# try to read file
+					cap = cv2.VideoCapture(path)
+					success, frame = cap.read() #TODO: refactor so that manually setting success isn't needed
+					if success:
+						# get dimensions of this video
+						height, width, _ = frame.shape
+						scaled_height, scaled_width = height/downsize_scale, width/downsize_scale
 
-					# parameters for displaying filter parameters on extracted frames
-					font = cv2.FONT_HERSHEY_SIMPLEX
-					font_position = (50, scaled_height - 40)
-					font_scale = .5
-					font_color = (0,0,255)
-					font_thickness = 2
-					
-					# dictionary of all extracted features to be displayed during runtime
-					features = {}
-					# dictionary of all binary masks used to extract those features
-					masks = {}
-					
-					# go through all the frames of this video, displaying extracted features
-					while success:
-						# rescale frame for faster processing
-						frame = cv2.resize(frame, (scaled_width, scaled_height))
-						hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+						# parameters for displaying filter parameters on extracted frames
+						font = cv2.FONT_HERSHEY_SIMPLEX
+						font_position = (50, scaled_height - 40)
+						font_scale = .5
+						font_color = (0,0,255)
+						font_thickness = 2
 						
-						features['raw'] = frame
+						# dictionary of all extracted features to be displayed during runtime
+						features = {}
+						# dictionary of all binary masks used to extract those features
+						masks = {}
 						
-						# get all masks and features for each filter
-						for key, (lower, upper) in self.filters.iteritems():
-							# create mask
-							masks[key] = cv2.inRange(hsv_frame, lower, upper)
-							# extract feature using mask
-							feature = cv2.bitwise_and(frame, frame, mask = masks[key])
+						# go through all the frames of this video, displaying extracted features
+						while success:
+							# rescale frame for faster processing
+							frame = cv2.resize(frame, (scaled_width, scaled_height))
+							hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 							
-							# write current filter info onto each filtered feature
-							lower_text = 'lower: [{0},{1},{2}]'.format(lower[0], lower[1], lower[2])
-							upper_text = 'upper: [{0},{1},{2}]'.format(upper[0],upper[1],upper[2])
-							cv2.putText(feature, lower_text, font_position, font, font_scale, font_color, font_thickness)
-							cv2.putText(feature, upper_text, (font_position[0], font_position[1]-30), font, font_scale, font_color, font_thickness)
+							features['raw'] = frame
+							
+							# get all masks and features for each filter
+							for key, (lower, upper) in self.filters.iteritems():
+								# create mask
+								masks[key] = cv2.inRange(hsv_frame, lower, upper)
+								# extract feature using mask
+								feature = cv2.bitwise_and(frame, frame, mask = masks[key])
+								
+								# write current filter info onto each filtered feature
+								lower_text = 'lower: [{0},{1},{2}]'.format(lower[0], lower[1], lower[2])
+								upper_text = 'upper: [{0},{1},{2}]'.format(upper[0],upper[1],upper[2])
+								cv2.putText(feature, lower_text, font_position, font, font_scale, font_color, font_thickness)
+								cv2.putText(feature, upper_text, (font_position[0], font_position[1]-30), font, font_scale, font_color, font_thickness)
 
-							features[key] = feature
+								features[key] = feature
 
-						# reset combined mask before recalculating it
-						# TODO: find a way around having to do this
-						masks['combined'] = 0
-						masks['combined'] = self.combine_masks(masks)
-						
-						features['combined'] = cv2.bitwise_and(frame, frame, mask = masks['combined'])
-						
-						# show all things
-						for key, feature in features.iteritems():
-							cv2.imshow(key, feature)
-						
-						# exit early if escape key pressed
-						k = cv2.waitKey(5) & 0xFF
-						if k == 27:
-							break
+							# reset combined mask before recalculating it
+							# TODO: find a way around having to do this
+							masks['combined'] = 0
+							masks['combined'] = self.combine_masks(masks)
+							
+							features['combined'] = cv2.bitwise_and(frame, frame, mask = masks['combined'])
+							
+							# show all things
+							for key, feature in features.iteritems():
+								cv2.imshow(key, feature)
+							
+							# exit early if escape key pressed
+							k = cv2.waitKey(5) & 0xFF
+							if k == 27:
+								break
 
-						# use defined keyboard controls to update upper and lower bounds of chosen color range
+							# use defined keyboard controls to update upper and lower bounds of chosen color range
 
-						# update lower bounds
-						for key, channel, sign, increment in self.lower_controls:
-							if k == ord(key):
-								lower_chosen[channel] = (lower_chosen[channel] + (sign * self.increments[increment])) % 256
-						# update upper bounds
-						for key, channel, sign, increment in self.upper_controls:
-							if k == ord(key):
-								upper_chosen[channel] = (upper_chosen[channel] + (sign * self.increments[increment])) % 256
-						
-						# keyboard controls to reset experimental color range
-						if k == ord('r'):
-							lower_chosen[:] = 0
-							upper_chosen[:] = 255
-						if k == ord('R'):
-							lower_chosen[:] = 0
-							upper_chosen[:] = 0
+							# update lower bounds
+							for key, channel, sign, increment in self.lower_controls:
+								if k == ord(key):
+									lower_chosen[channel] = (lower_chosen[channel] + (sign * self.increments[increment])) % 256
+							# update upper bounds
+							for key, channel, sign, increment in self.upper_controls:
+								if k == ord(key):
+									upper_chosen[channel] = (upper_chosen[channel] + (sign * self.increments[increment])) % 256
+							
+							# keyboard controls to reset experimental color range
+							if k == ord('r'):
+								lower_chosen[:] = 0
+								upper_chosen[:] = 255
+							if k == ord('R'):
+								lower_chosen[:] = 0
+								upper_chosen[:] = 0
 
-						# advance frame
-						success, frame = cap.read()
-						
-					cv2.destroyAllWindows()
-					
-					# update values of this filter if desired
-					if (write):
-						self.filters[chosen][0], self.filters[chosen][1] = lower_chosen, upper_chosen
-					return (lower_chosen, upper_chosen)
-				# TODO: implement actual exception/error handling
-				else: print 'The file provided could not be read. Make sure the given path leads to a video file.'
+							# advance frame
+							success, frame = cap.read()
+					# TODO: implement actual exception/error handling
+					else: 
+						print 'The file provided could not be read. Make sure the given path leads to a video file.'
+						return None
+					print "Satisfied with current filter params for {0}? (y/n)".format(chosen)
+					done = raw_input()
+				cv2.destroyAllWindows()
+				
+				# update values of this filter if desired
+				if (write):
+					self.filters[chosen][0], self.filters[chosen][1] = lower_chosen, upper_chosen
+				return [lower_chosen, upper_chosen]
+
 			else: 
 				print ('The desired filtering parameter \'{0}\' was not found '+
 				'in the parameter set:\r\n{1}').format(chosen, self.filters.keys())
 		else: print 'The supplied filepath \'{0}\' did not lead to a file.'.format(path)
 	
+	# do find_filter on all supplied filters, or all filters by default
+	def find_filters(self, path, chosen = None, downsize_scale = 2):
+		if chosen is not None:
+			filters = chosen
+		else: filters = self.filters
+		for key in filters:
+			print 'currently finding: {0}'.format(key)
+			derived = self.find_filter(path, key, downsize_scale = downsize_scale)
+			print '\t = {0}'.format(derived)
+		return self.export_params()
+
 	# return a set of predetermined filters based on the trials using Steven's skateboard
 	def get_default_filters(self):
 		return {'board': [np.array([82,39,80]), np.array([100,155,180])], 
@@ -170,7 +187,7 @@ class FilterFinder:
 			'edge': [np.array([10,25,115]), np.array([25,155,255])]}
 	
 	# map a single name, or list of names to a default set of filter parameters
-	def get_filter_map(self, names, min_style_default = True):
+	def add_filter_maps(self, names, min_style_default = True):
 		# decide which range to copy into all filters by default
 		default = []
 		if min_style_default is False:
